b4836212b87f40e7476472ac9db6066045f6c1e1
christophe <christophe@oxio.ca>
2022-03-04 13:39:43 -0500

    update jest

--- src/test/error.spec.ts
+++ src/test/error.spec.ts
@@ -3,7 +3,10 @@ import request from 'supertest';
 import { app } from '../app';
 
 describe('Error page', () => {
-  it('should return 404 for not existing page', () => {
-    return request(app).get('/fake-page').expect(404);
+  it('should return 404 for not existing page', (done) => {
+    request(app).get('/fake-page').end((err, res) => {
+      expect(res.statusCode).toBe(404);
+      done();
+    });
   });
 });
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -3,14 +3,19 @@ import request from 'supertest';
 import { app } from '../app';
 
 describe('GET /', () => {
-  it('should return 200 OK', () => {
-    return request(app).get('/').expect(200);
+  it('should return 200 OK', (done) => {
+    request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
   });
 
   it('should return Hello Express', (done) => {
-    return request(app)
+    request(app)
       .get('/')
-      .end(function (err, res) {
+      .end((err, res) => {
         expect(res.text).toContain('Hello Express');
         done();
       });
